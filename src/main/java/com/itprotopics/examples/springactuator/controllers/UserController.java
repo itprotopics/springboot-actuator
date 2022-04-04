da7d4f255877ad9852c6ff986caf223860cf2064
José Luis Alavez Manzano <jose.alavez@softtek.com>
2022-04-04 11:21:21 -0500

    Se sube codigo base

--- src/main/java/com/itprotopics/examples/springactuator/controllers/UserController.java
+++ src/main/java/com/itprotopics/examples/springactuator/controllers/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/users")
+@RequestMapping("/msuser-manager/v1/users")
 public class UserController {
   
   @Autowired
